3f97053c80569edc50f67f8f87ed3e16968d1992
y@su <toyasyu@gmail.com>
2016-01-14 03:12:54 +0900

    Add rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'guard/haml_lint'
+require 'guard/ui'
 require 'guard/compat/test/helper'
+require 'guard/haml_lint'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
