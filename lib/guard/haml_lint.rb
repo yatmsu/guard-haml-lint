00fe202fbb60561144c46b506aa58e88700906c5
y@su <toyasyu@gmail.com>
2018-05-03 22:08:55 +0900

    add `frozen_string_literal: true`

--- lib/guard/haml_lint.rb
+++ lib/guard/haml_lint.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'guard/compat/plugin'
 require 'shellwords'
 
--- lib/guard/hamllint/version.rb
+++ lib/guard/hamllint/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Guard
   # guard-haml_lint version class
   class HamlLintVersion
